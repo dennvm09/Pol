a6556fd3af23ccac332593a9adb10cfd40c8e648
dennvm09 <valenmosquera1999@gmail.com>
2019-02-17 11:55:07 -0500

    Método de bisección listo.

--- src/application/PolynomialsController.java
+++ src/application/PolynomialsController.java
@@ -46,24 +46,61 @@ public class PolynomialsController {
 	private Button btSave;
 	@FXML
 	private TextArea txtRoots;
+    @FXML
+    private Button btSave1;
+    @FXML
+    private TextArea txtRoots1;
+    @FXML
+    private TextField txtx101;
+    @FXML
+    private TextField txtx91;
+    @FXML
+    private TextField txtx81;
+    @FXML
+    private TextField txtx71;
+    @FXML
+    private TextField txtx61;
+    @FXML
+    private TextField txtx51;
+    @FXML
+    private TextField txtx41;
+    @FXML
+    private TextField txtx31;
+    @FXML
+    private TextField txtx21;
+    @FXML
+    private TextField txtx12;
+    @FXML
+    private TextField txtx111;
+    @FXML
+    private Label lblPolynomial1;
+	@FXML
+	private TextField txtMin;
+	@FXML
+	private TextField txtMax;
 	
+	
+	
+
 	public void initialize() {}
-	
-	
-	
 	
 	@FXML
 	public void showFunction(ActionEvent e) {
 		System.out.println("Hola");
-		saveFunction(e);
+		
+		
+		if(tabOption1.equals("Opcion 1")) {
+			saveFunction(e);
+		}else {
+			saveFunction1(e);
+		}
+		
 	}
-	
 	
 	public void saveFunction(ActionEvent e) {
 	
 		//System.out.println("Hola");
 		String function = "";
-		
 		String x1 = txtx1.getText();
 		String x2 = txtx2.getText();
 		String x3 = txtx3.getText();
@@ -75,8 +112,6 @@ public class PolynomialsController {
 		String x9 = txtx9.getText();
 		String x10 = txtx10.getText();
 		String x11 = txtx11.getText();
-		
-		
 		
 		if(x10.isEmpty()) {
 			function += "";
@@ -142,12 +177,89 @@ public class PolynomialsController {
 		}
 		
 		System.out.println(function);
-		
 		lblPolynomial.setText(function);
-		
 	}
 	
-	
+	public void saveFunction1(ActionEvent e) {
+		String function = "";
+		String x1 = txtx11.getText();
+		String x2 = txtx21.getText();
+		String x3 = txtx31.getText();
+		String x4 = txtx41.getText();
+		String x5 = txtx51.getText();
+		String x6 = txtx61.getText();
+		String x7 = txtx71.getText();
+		String x8 = txtx81.getText();
+		String x9 = txtx91.getText();
+		String x10 = txtx101.getText();
+		String x11 = txtx111.getText();
+		
+		if(x10.isEmpty()) {
+			function += "";
+		}else {
+			function += x10+"x^10+"; 	
+		}
+		
+		if(x9.isEmpty()) {
+			function += "";
+		}else {
+			function += x9+"x^9+";
+		}
+		if(x8.isEmpty()) {
+			function += "";
+		}else {
+			function += x8+"x^8+";
+		}
+		if(x7.isEmpty()) {
+			function += "";
+		}else {
+			function += x7+"x^7+";
+		}
+		if(x6 != null) {
+			function += "";
+		}else {
+			function += x6+"x^6+";
+		}
+		
+		if(x5.isEmpty()) {
+			function += "";
+		}else {
+			function += x5+"x^5+";
+		}
+		if(x4.isEmpty()) {
+			function += "";
+		}else {
+			function += x4+"x^4+";
+		}
+		if(x3.isEmpty()) {
+			function += "";
+		}else {
+			function += x3+"x^3+";
+		}
+		if(x2.isEmpty()) {
+			function += "";
+		}else {
+			function += x2+"x^2+";
+		}
+		if(x1.isEmpty()) {
+			function += "";
+		}else {
+			function += x1+"x+";
+		}
+		if(x11.isEmpty()) {
+			function += "0";
+		}else {
+			function += x11;
+		}
+		
+		if(x1.isEmpty() && x2.isEmpty() && x3.isEmpty() && x4.isEmpty() && x5.isEmpty()
+				&& x6.isEmpty() && x7.isEmpty() && x8.isEmpty() && x9.isEmpty() && x10.isEmpty() && x11.isEmpty()) {
+			System.out.println("No has escrito ninguna función.");
+		}
+		
+		System.out.println(function);
+		lblPolynomial1.setText(function);
+	}
 	
 	
 }
--- src/model/Polynomials.java
+++ src/model/Polynomials.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 
 public class Polynomials {
 
-	private int polynomialGrade;
-	private ArrayList<Integer> coeficientes;
+//	private int polynomialGrade;
+//	private ArrayList<Integer> coeficientes;
+	
+	//Principal
+	
+	
+	private Function function;
 	
 
 	public Polynomials() {
@@ -13,36 +18,66 @@ public class Polynomials {
 	}
 	
 	
-
-	
-	
 	public double bisectionMethod(String f, int min, int max) {
+		System.out.println("Entró");
+		double mi, ma, mm;
+		double x = Double.NaN;
+		int r = 0;
 		
+		function = new Function(f);
+		mi = function.f(min);
+		ma = function.f(max);
+		
+		if(mi * ma < 0) {
+			System.out.println("Entró 1");
+			do {
+				r++;
+				System.out.println("Entró al do");
+				x = (mi + ma) / 2;
+				mm = function.f(x);
+				if(mi * mm < 0) {
+					ma = x;
+				}else {
+					mi = x;
+				}
+			}while(Math.abs(mm) > 0.001 && r <= 100);
+		}
+		return x;
+	}
+	
+	
+	public double newtonRaphson(String f, int p0) {	
 		
 		return 0;
 	}
 	
 	
-	public double newtonRaphson() {	
-		
-		return 0;
+	
+	public static void main(String[] args) {
+		Polynomials p = new Polynomials();
+		String f = "x-4";
+		int min = 0, max = 5;
+		System.out.println("Hola");
+		System.out.println(p.bisectionMethod(f, min, max));
+		System.out.println("Hola");
 	}
 	
-	 int getPolynomialGrade() {
-		return polynomialGrade;
-	}
-
-	public void setPolynomialGrade(int polynomialGrade) {
-		this.polynomialGrade = polynomialGrade;
-	}
-
-	public ArrayList<Integer> getCoeficientes() {
-		return coeficientes;
-	}
-
-	public void setCoeficientes(ArrayList<Integer> coeficientes) {
-		this.coeficientes = coeficientes;
-	} 
 	
-	
+//	 int getPolynomialGrade() {
+//		return polynomialGrade;
+//	}
+//
+//	public void setPolynomialGrade(int polynomialGrade) {
+//		this.polynomialGrade = polynomialGrade;
+//	}
+//
+//	public ArrayList<Integer> getCoeficientes() {
+//		return coeficientes;
+//	}
+//
+//	public void setCoeficientes(ArrayList<Integer> coeficientes) {
+//		this.coeficientes = coeficientes;
+//	} 
+//	
+//	
 }
